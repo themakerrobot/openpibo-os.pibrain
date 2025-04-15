86e07e8b7e52f6a5515c9b056c3fd9440546c3e8
leeyunjai82 <leeyunjai@circul.us>
2025-04-15 14:31:15 +0900

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,4 +74,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_favicon = '_static/icon.png'
 html_css_files = ['mycss.css']
