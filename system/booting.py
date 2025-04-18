c69f9469e0f4bbba2a3268e6790c558cf1b31ed3
leeyunjai82 <leeyunjai@circul.us>
2025-04-18 12:10:13 +0900

    update

--- system/booting.py
+++ system/booting.py
@@ -149,8 +149,7 @@ def boot():
     if (tmp[6] != '' and tmp[6][0:3] != '169') or (tmp[7] != '' and tmp[7][0:3] != '169'):
       os.system("/home/pi/openpibo-os/system/hotspot.sh stop")
       break
-    ole.draw_image("/home/pi/openpibo-os/system/pibrain320.jpg")
-    ole.draw_text((5,5), "˚".join(["" for _ in range(i+1)]))
+    ole.draw_text((5,5), "-".join(["" for _ in range(i+1)]))
     ole.show()
     time.sleep(3)
   network_disp.run()
